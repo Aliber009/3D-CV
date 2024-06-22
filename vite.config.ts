8f515f49d3c9af47939e205d3fcb64a034db4936
Aliber009 <aliberoro@gmail.com>
2024-06-23 00:11:48 +0200

    update build final

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from '@vitejs/plugin-legacy';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+        targets: ['defaults', 'not IE 11']
+    }),
+    viteStaticCopy({
+        targets: [
+            {
+                src: 'public/.nojekyll',
+                dest: '.'
+            }
+        ]
+    })
+],
   base: '/3D-CV/'
 });
