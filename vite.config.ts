8c856fb28350f35b91c5c31caef3dab457126bde
Aliber009 <aliberoro@gmail.com>
2024-06-23 03:30:07 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import legacy from '@vitejs/plugin-legacy';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy({
-        targets: ['defaults', 'not IE 11']
-    }),
-    viteStaticCopy({
-        targets: [
-            {
-                src: 'public/.nojekyll',
-                dest: '.'
-            }
-        ]
-    })
 ],
   
 });
